1e7778c31d76f318d4c00ca1fa82c6b49ad3db8d
Vishruth S <vs.vishruth.s@gmail.com>
2020-10-18 19:15:30 +0530

    Added infix to postfix converter

---
+++ INFIX_TO_POSTFIX_CONVERTER/PostfixConverter.cpp
@@ -0,0 +1,89 @@
+// THIS IS A PROGRAM THAT TAKES AN INFIX EXPRESSION AND CONVERTS IT INTO POSTFIX EXPRESSION
+// Done by - Vishruth S, https://vishruth-s.github.io/Me/
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// Function to get precedence of character
+int getPrecedence(char c)
+{
+    switch (c)
+    {
+    case '^':
+        return 3;
+    case '/':
+    case '*':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+    default:
+        return -1;
+    }
+}
+
+// Function to check if opening bracket
+bool isOpeningBracket(char c)
+{
+    return c == '(' || c == '{' || c == '[';
+}
+
+// Function to check if closing bracket
+bool isClosingBracket(char c)
+{
+    return c == ')' || c == '}' || c == ']';
+}
+
+int main()
+{
+    string s; //infix expression string
+    cout << "\nEnter infix expression  ";
+    getline(cin, s);
+
+    stack<char> operators; //operator stack
+    vector<char> postfix;  //postfix array
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (isalpha(s[i])) //check if operand
+        {
+            postfix.push_back(s[i]);
+        }
+        else if (!isOpeningBracket(s[i]) && !isClosingBracket(s[i])) //if operator
+        {
+            while (!operators.empty() && !isOpeningBracket(operators.top()) && getPrecedence(s[i]) <= getPrecedence(operators.top()))
+            {
+                postfix.push_back(operators.top());
+                operators.pop();
+            }
+            operators.push(s[i]);
+        }
+        else if (isOpeningBracket(s[i])) //if opening parenthesis
+        {
+            operators.push(s[i]);
+        }
+        else if (isClosingBracket(s[i])) //if closing parenthesis
+        {
+            while (!operators.empty() && !isOpeningBracket(operators.top()))
+            {
+                postfix.push_back(operators.top());
+                operators.pop();
+            }
+            operators.pop();
+        }
+    }
+    while (!operators.empty())
+    {
+        postfix.push_back(operators.top());
+        operators.pop();
+    }
+    cout << "\nPostfix Expression: ";
+    for (int i = 0; i < postfix.size(); i++)
+    {
+        cout << postfix[i];
+    }
+}
