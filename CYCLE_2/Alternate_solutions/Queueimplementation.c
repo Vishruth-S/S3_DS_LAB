8fa82c20eb0badce176d685095e46bd36f689249
Vishruth Subramanian <vs.vishruth.s@gmail.com>
2020-11-02 17:04:04 +0530

    Fixed bugs

--- CYCLE_2/Alternate_solutions/Queueimplementation.c
+++ CYCLE_2/Alternate_solutions/Queueimplementation.c
@@ -3,15 +3,20 @@
 #include<stdio.h>
 int rear=-1,front=-1;
 int max=5;
+
 int isfull() 
 {
     if(rear==max-1)
         return 1;
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
+int isempty()
+{
+    if(front==-1||front>rear)
+        return 1;
+     return 0;
+}
+
 void enq(int q[],int e)
 {
     if(isfull())
@@ -28,15 +33,7 @@ void enq(int q[],int e)
     }
     
 }
-int isempty()
-{
-    if(front==-1||front>rear)
-        return 1;
-    else
-    {
-        return 0;
-    }
-}
+
 void deq(int q[])
 {
     if(!isempty())
@@ -58,13 +55,11 @@ void display(int q[])
     for(int i=rear;i>=front;i--)
         printf("%d\n",q[i]);
 }
+
 void main()
 {
-     int q[5],n,ch,e;
-    char ch1;
-    ch1='y'; 
-    while(ch1=='y')
-    {
+     int q[5],ch=4,e;
+    do {
         printf("\n1.Push\n2.Pop");
         scanf("%d",&ch);
         if(ch==1)
@@ -72,14 +67,14 @@ void main()
             printf("Enter the element you have to push");
             scanf("%d",&e);
             enq(q,e);
-            display(q);
         }
         if(ch==2)
         {
             deq(q);
+        }
+        if(ch==3)
+        {
             display(q);
         }
-        
-    }
-
+    } while(ch<=3);
 }
