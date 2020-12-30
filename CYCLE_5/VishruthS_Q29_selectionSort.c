4f5ad4c169f3527c8b41984093689394c5eaf399
Vishruth S <vs.vishruth.s@gmail.com>
2020-12-30 19:01:26 +0530

    Added program 29

---
+++ CYCLE_5/VishruthS_Q29_selectionSort.c
@@ -0,0 +1,39 @@
+//CSL201 DATA STRUCTURES LAB ----- VISHRUTH S, CS3A, 61
+//CYCLE 5 QUESTION 1
+//Program to implement Selection Sort
+
+#include <stdio.h>
+
+void selectionSort(int arr[], int size)
+{
+    int minIndex, temp;
+    for (int i = 0; i < size; i++) // move starting point of unsorted array one-by-one
+    {
+        minIndex = i;
+        for (int j = i + 1; j < size; j++) // find the minimum in unsorted array
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+
+        temp = arr[minIndex]; // swap minimum with first element
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main()
+{
+    int arr[100], size;
+    printf("\nEnter size ");
+    scanf("%d", &size);
+    printf("\nEnter Array "); 
+    for (int i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+
+    selectionSort(arr, size);  
+
+    printf("Sorted Array: \n");
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
