e5b5f1c46fc83dd640f0513762b59d29c42e3351
Vishruth S <vs.vishruth.s@gmail.com>
2021-01-05 20:56:37 +0530

    Added cycle 6

---
+++ CYCLE_6/VishruthS_Q34_BST-Traversal.c
@@ -0,0 +1,107 @@
+//CSL201 DATA STRUCTURES LAB ----- VISHRUTH S, CS3A, 61
+//CYCLE 6 QUESTION 1
+//Program  to implement a binary search tree
+//and perform inorder, preorder, & postorder Traversing 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary tree Node structure
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+// ======= Binary Search tree TRAVERSAL ======= //
+// Preorder Traversal
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Inorder Traversal
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+// Postorder Traversal
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+struct Node *insertNode(struct Node *, int);
+struct Node *createNode(int);
+
+// ======== Main function ========= //
+int main()
+{
+    struct Node *root = NULL;
+    int ch, inputData;
+    do
+    {
+        printf("\n\n1.Insert\n2.Preorder\n3.Inorder\n4.PostOrder\n");
+        printf("\nEnter choice  ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter data to be inserted: ");
+            scanf("%d", &inputData);
+            root = insertNode(root, inputData);
+            break;
+        case 2:
+            printf("\nPreorder Traversal\n");
+            preorder(root);
+            break;
+        case 3:
+            printf("\nInorder Traversal\n");
+            inorder(root);
+            break;
+        case 4:
+            printf("\nPostorder Traversal\n");
+            postorder(root);
+            break;
+        }
+    } while (ch < 5);
+
+    return 0;
+}
+
+// ====== Additional functions ====== //
+// Function to insert a newNode
+struct Node *insertNode(struct Node *root, int data)
+{
+    if (root == NULL)
+        root = createNode(data);
+    else if (data <= root->data)
+        root->left = insertNode(root->left, data);
+    else
+        root->right = insertNode(root->right, data);
+
+    return root;
+}
+// Function to create a new node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
---
+++ CYCLE_6/VishruthS_Q35_BST-Operations.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Binary Search Tree node structure
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int);
+struct Node *FindMinimum(struct Node *);
+void inorder(struct Node *);
+
+// ======= To insert a newNode ======= //
+// Time complexity: O(logn)
+struct Node *insertNode(struct Node *root, int data)
+{
+    if (root == NULL)                                // if root is null,
+        root = createNode(data);                     // then a new node is created
+    else if (data <= root->data)                     // if data is less than current data,
+        root->left = insertNode(root->left, data);   // then node to be inserted in left sub-tree
+    else                                             //else if data is greater than current data
+        root->right = insertNode(root->right, data); // then node to be inserted in right sub-tree
+
+    return root;
+}
+
+// ===== To search for a node ======= //
+// Time complexity: O(logn)
+bool Search(struct Node *root, int data)
+{
+    if (root == NULL) // if it reaches null, it means element doesn't exists
+        return false;
+    if (root->data == data) // if data is equal to search Query, then return true
+        return true;
+
+    else if (data <= root->data)          // if data is less than current data
+        return Search(root->left, data);  // then continue searching in left sub-tree
+    else                                  // else if data is greater than current data
+        return Search(root->right, data); // then continue searching in right sub-tree
+}
+
+// ===== To delete a node ======== //
+// Time complexity: O(logn)
+struct Node *Delete(struct Node *root, int data)
+{
+    if (root == NULL) // if empty node, then return it;
+        return root;
+    else if (data < root->data)                  // if data less than current data
+        root->left = Delete(root->left, data);   // then search for it in left sub-tree
+    else if (data > root->data)                  // else if data is greater than current data
+        root->right = Delete(root->right, data); // then search for it in right sub-tree
+
+    else // once it reaches here, it means the current node is the node to be deleted
+    {
+        // case 1: Leaf node, No child
+        if (root->left == NULL && root->right == NULL)
+        {
+            free(root);  // delete it
+            root = NULL; // and set current value to null
+        }
+        // Case 2: Only one child
+        else if (root->left == NULL) // only Left child
+        {
+            struct Node *temp = root; // current node
+            root = root->right;       // change link to that of next node
+            free(temp);               // and delete the current node
+        }
+        else if (root->right == NULL) // or only Right child
+        {
+            struct Node *temp = root; // current node
+            root = root->left;        // change link to that of next node
+            free(temp);               // and delete the current node
+        }
+        // Case 3: Both children
+        else
+        {
+            struct Node *temp = FindMinimum(root->right);  // find minimum of right node (can also be replaced with FindMaximum(root->left))
+            root->data = temp->data;                       // and set current node equal to that value
+            root->right = Delete(root->right, temp->data); // and delete that node
+        }
+    }
+    return root;
+}
+
+// ======= MAIN function ======== //
+int main()
+{
+    struct Node *root = NULL;
+    int ch, data, queryData;
+    printf("\nBinary Search Tree");
+    do
+    {
+        printf("\n\n1.Insert\n2.Search\n3.Delete\n4.Display (Inorder)\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter value to be inserted\n");
+            scanf("%d", &data);
+            root = insertNode(root, data);
+            break;
+        case 2:
+            printf("\nEnter value to be searched\n");
+            scanf("%d", &queryData);
+            if (Search(root, queryData))
+                printf("\nElement found ");
+            else
+                printf("\nElement not found ");
+            break;
+        case 3:
+            printf("\nEnter value to be deleted\n");
+            scanf("%d", &queryData);
+            root = Delete(root, queryData);
+            printf("\nElement deleted successfully (if it exists)");
+            break;
+        case 4:
+            printf("\nBinary Search Tree Inorder\n");
+            inorder(root);
+        }
+    } while (ch < 5);
+
+    return 0;
+}
+
+// ====== Additional helper functions ======= //
+// Function to create a new node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+}
+
+// Function to find minimum value node in a sub-tree
+struct Node *FindMinimum(struct Node *root)
+{
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+// For Inorder Traversal
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
